185abbe9a7f51c646162c20edddc9f4814732778
Tal Sagie <talsagie19@gmail.com>
2024-11-16 17:35:00 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ghPages } from "vite-plugin-gh-pages";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
